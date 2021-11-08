1722c93154b5fa3a49ebaf49754b0064bc16757e
hedwards <hgedwards87@gmail.com>
2021-11-08 15:45:15 -0800

    add first two enumerable problems

--- D1/Enumerables.rb
+++ D1/Enumerables.rb
@@ -1,7 +1,41 @@
-def hello
-    puts 'Hello'
+class Array
+
+    def my_each(&prc)
+        i = 0
+        while i < self.length
+            prc.call(self[i])
+
+            i += 1
+        end
+        self
+    end
+
+    def my_select(&prc)
+        results = []
+        
+        # self.my_each(
+        # results << self if self.my_each(prc) == true
+
+        results
+    end
+
+
 end
 
-def boo
-    puts 'boo'
-end
+
+            # return_value = [1, 2, 3].my_each do |num|
+            #   puts num
+            # end.my_each do |num|
+            #   puts num
+            # end
+            # # => 1
+            #     #  2
+            #     #  3
+            #     #  1
+            #     #  2
+            #     #  3
+
+            # p return_value  # => [1, 2, 3]
+            # return_value
+
+    
