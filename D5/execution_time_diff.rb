826a80b4864982b7935a6242c9c08f474d8c916b
Kirti-Harode <kikihrd20@gmail.com>
2021-11-19 15:54:57 -0800

    almost there

--- D5/execution_time_diff.rb
+++ D5/execution_time_diff.rb
@@ -66,18 +66,30 @@ end
 #   p  largest_contiguous_sub_sum(list)
 
 def largest_contiguous_sub_sum2(arr)
-    largest_sum=-10
-    current_sum=-10
-    
-   
+    largest_sum = arr.max
+    current_sum = arr[0]
+    min = arr.min
+    i = 1
+    while i < arr.length - 1
+        current_sum += arr[i]
+        if current_sum <= min 
+            current_sum = arr[i + 1]
+        end
+        if current_sum > largest_sum 
+            largest_sum = current_sum
+        end
+        
+        i += 1
+    end
+   return largest_sum
 end
 
 # list = [-5, -1, -3]
 #   p  largest_contiguous_sub_sum2(list)
 #     list = [5, 3, -7]
 #   p  largest_contiguous_sub_sum2(list)
-#   list = [2, 3, -6, 7, -6, 7]
-#   p  largest_contiguous_sub_sum2(list) # => 8 (from [7, -6, 7]
+  list = [2, 3, -6, 7, -6, 7]
+  p  largest_contiguous_sub_sum2(list) # => 8 (from [7, -6, 7]
 # a = [1,2,3,4]
 # sums=[]
 #     (1...a.length).each do |i|
@@ -87,28 +99,28 @@ end
 
 # p a.combination(1).to_a
 
-def largest_contiguous_sub_sum2(arr)
-    largest_sum=-10
-    current_sum=-10
-    combinations=[]
-    i=0
-    a = arr.length 
-    while i < arr.length 
-        combinations << arr[i..a-1]
-        i+=1
-    end
-p combinations
-    combinations.each do |sub|
-        current_sum = sub.sum 
-        if current_sum > largest_sum 
-            largest_sum = current_sum
-        end
-    end
-    return largest_sum
-end
+# def largest_contiguous_sub_sum2(arr)
+#     largest_sum=-10
+#     current_sum=-10
+#     combinations=[]
+#     i=0
+#     a = arr.length 
+#     while i < arr.length 
+#         combinations << arr[i..a-1]
+#         i+=1
+#     end
+# p combinations
+#     combinations.each do |sub|
+#         current_sum = sub.sum 
+#         if current_sum > largest_sum 
+#             largest_sum = current_sum
+#         end
+#     end
+#     return largest_sum
+# end
 
 # list = [2, 3, -6, 7, -6, 7]
 #   p  largest_contiguous_sub_sum2(list)
-  list = [-5, -1, -3]
-  p  largest_contiguous_sub_sum2(list)
+#   list = [-5, -1, -3]
+#   p  largest_contiguous_sub_sum2(list)
 
