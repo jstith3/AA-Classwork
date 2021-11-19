4a044725280a683499fc7f80759f8e33723a820c
Justin Stith <justinbluejstith@gmail.com>
2021-11-19 18:06:59 -0500

    new phase 2

--- D5/execution_time_diff.rb
+++ D5/execution_time_diff.rb
@@ -39,6 +39,76 @@ end
 
 
 def largest_contiguous_sub_sum(arr)
+    all_subarray=[]
     
-
+    (0...arr.length).each do |i|
+        (0...arr.length).each do |j|
+            all_subarray << arr[i..j]
+        end
+    end
+    # p all_subarray
+    sum=[]
+    all_subarray.each do |sub|
+        if !sub.empty?
+        sum<< sub.sum
+        end
+    end
+    sum.max
 end
+# Time complexity (n + n^2 => n^2)
+
+
+# #   list = [5, 3, -7]
+# #   p  largest_contiguous_sub_sum(list)
+# #   list = [2, 3, -6, 7, -6, 7]
+# #   p  largest_contiguous_sub_sum(list) # => 8 (from [7, -6, 7])
+#   list = [-5, -1, -3]
+#   p  largest_contiguous_sub_sum(list)
+
+def largest_contiguous_sub_sum2(arr)
+    largest_sum=-10
+    current_sum=-10
+    
+   
+end
+
+# list = [-5, -1, -3]
+#   p  largest_contiguous_sub_sum2(list)
+#     list = [5, 3, -7]
+#   p  largest_contiguous_sub_sum2(list)
+#   list = [2, 3, -6, 7, -6, 7]
+#   p  largest_contiguous_sub_sum2(list) # => 8 (from [7, -6, 7]
+# a = [1,2,3,4]
+# sums=[]
+#     (1...a.length).each do |i|
+#         sums+= a.combination(i).to_a
+#     end
+#     return 
+
+# p a.combination(1).to_a
+
+def largest_contiguous_sub_sum2(arr)
+    largest_sum=-10
+    current_sum=-10
+    combinations=[]
+    i=0
+    a = arr.length 
+    while i < arr.length 
+        combinations << arr[i..a-1]
+        i+=1
+    end
+p combinations
+    combinations.each do |sub|
+        current_sum = sub.sum 
+        if current_sum > largest_sum 
+            largest_sum = current_sum
+        end
+    end
+    return largest_sum
+end
+
+# list = [2, 3, -6, 7, -6, 7]
+#   p  largest_contiguous_sub_sum2(list)
+  list = [-5, -1, -3]
+  p  largest_contiguous_sub_sum2(list)
+
