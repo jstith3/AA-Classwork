5ed3d9b0fddd3283446cd9115afb10a82402d254
Justin Stith <justinbluejstith@gmail.com>
2021-11-24 17:53:23 -0500

    add enrollment

--- Week 5/W5D4/skeleton/skeleton/app/models/course.rb
+++ Week 5/W5D4/skeleton/skeleton/app/models/course.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  instructor_id :integer
+#  prereq_id     :integer
+#
 class Course < ApplicationRecord
+    has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
 end
--- Week 5/W5D4/skeleton/skeleton/app/models/enrollment.rb
+++ Week 5/W5D4/skeleton/skeleton/app/models/enrollment.rb
@@ -1,2 +1,27 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  course_id  :integer
+#  student_id :integer
+#
+# create_table "enrollments", force: :cascade do |t|
+#     t.integer "course_id"
+#     t.integer "student_id"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+
 class Enrollment < ApplicationRecord
+    belongs_to :course,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
+
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
 end
