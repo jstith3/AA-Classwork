89a81c700c6c6f299c74bddd32e125fd2e0fa516
ian <ianfarr44@gmail.com>
2021-12-06 15:28:40 -0800

    password hashing

--- Week 7/Week 7 Day 1/skeleton/skeleton/app/models/user.rb
+++ Week 7/Week 7 Day 1/skeleton/skeleton/app/models/user.rb
@@ -17,7 +17,24 @@ class User < ApplicationRecord
         self.session_token ||= SecureRandom::urlsafe_base64
     end
 
-    
-   
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(password_digest)
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(username: user_name)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
 
 end
