3df2ad3821030448556ddc0f1fbfcf8a5a98f2df
Justin Stith <justinbluejstith@gmail.com>
2021-12-06 19:25:57 -0500

    create new user view and user controller

--- Week 7/Week 7 Day 1/skeleton/skeleton/app/controllers/users_controller.rb
+++ Week 7/Week 7 Day 1/skeleton/skeleton/app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
-  
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            redirect_to user_url(@user.id)
+            # redirects to user page
+        else
+            render json: @user.errors.full_messages, status: 422 
+        end
+
+    end
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+
+
+
+    private
+    def user_params
+        params.require(:user).permit(:user_name, :password)
+    end
 end
