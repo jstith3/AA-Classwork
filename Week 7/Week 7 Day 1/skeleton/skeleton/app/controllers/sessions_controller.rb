f87ef2f9655942202e708d6736ad3ad655f47a6a
Justin Stith <justinbluejstith@gmail.com>
2021-12-06 20:26:23 -0500

    complete sessions

--- Week 7/Week 7 Day 1/skeleton/skeleton/app/controllers/sessions_controller.rb
+++ Week 7/Week 7 Day 1/skeleton/skeleton/app/controllers/sessions_controller.rb
@@ -1,10 +1,24 @@
 class SessionsController < ApplicationController
-    
+    before_action :require_logged_out, only: [:new, :create]
+    # only run new and create when you're logged out 
+    before_action :require_logged_in, only: [:destroy]
+    # only run destroy when you're logged in 
   def new
   end
 
   def create
-    @user = User.find_by_credentials(params[:user][:user_name], params[:user])
+    @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
+    if @user 
+        login(@user)
+        redirect_to user_url(@user)
+    else
+        render :new
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
   end
 
 end
